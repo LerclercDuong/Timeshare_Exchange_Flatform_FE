b5b5e1743ad810a546a82e6467fcdfd7ba9da771
Xtinh <daoxuantinh1592003@gmail.com>
2024-01-16 21:22:52 +0700

    Homepage_2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
-//import lib here
 import React from 'react'
-import './footer.css';
+import './footer.css'
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png';
@@ -24,23 +23,12 @@ const quick_link = [
   },
 ];
 
-const quick_links2 = [
-  {
-    path: '/login',
-    display: 'Login'
-  },
-  {
-    path: '/register',
-    display: 'Register'
-  },
-];
 
-const Footer = () =>{
+const Footer = () => {
 
-    const year = new Date().getFullYear()
+  const year = new Date().getFullYear()
 
-    //write code here
-return <footer className='footer'>
+  return <footer className='footer' >
     <Container>
       <Row>
         <Col lg='3'>
@@ -60,7 +48,7 @@ return <footer className='footer'>
             </div>
           </div>
         </Col>
-        <Col lg='3'>
+        <Col lg='6'>
           <h5 className='footer__link-title'>Discover</h5>
           <ListGroup className='footer__quick-links'>
             {
@@ -72,19 +60,8 @@ return <footer className='footer'>
             }
           </ListGroup>
         </Col>
-        <Col lg='3'>
-          <h5 className='footer__link-title'>Quick Links</h5>
-          <ListGroup className='footer__quick-links'>
-            {
-              quick_link.map((item, index) => (
-                <ListGroupItem key={index} className='ps-0 border-0'>
-                  <Link to={item.path}>{item.display}</Link>
-                </ListGroupItem>
-              ))
-            }
-          </ListGroup>
-        </Col>
-        <Col lg='3'>
+        
+        <Col lg='2'>
           <h5 className='footer__link-title'>Contacts</h5>
           <ListGroup className='footer__quick-links'>
             <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
@@ -124,4 +101,4 @@ return <footer className='footer'>
   </footer>
 }
 
-export default Footer;
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/home.css'
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../styles/home.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { Container, Row, Col } from 'reactstrap';
 import heroImg from '../assets/images/hero-img01.jpg';
 import heroImg02 from '../assets/images/hero-img02.jpg';
@@ -10,6 +11,10 @@ import heroVideo from '../assets/images/hero-video.mp4';
 import Subtitle from '../shared/Subtitle';
 import worldImg from '../assets/images/world.png';
 import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
+import experienceImg from '../assets/images/experience.png';
+import Testimonial from '../components/Testimonial/Testimonial';
 
 const Home = () => {
   return (
@@ -53,8 +58,75 @@ const Home = () => {
     <Container>
       <Row>
         <Col lg='3'>
-          <h5 className='service__subtitle'> What we serve</h5>
-          <h2 className='services'>We offer our best services</h2>
+          <h5 className='services__subtitle'> What we serve</h5>
+          <h2 className='services__title'>We offer our best services</h2>
+        </Col>
+        <ServiceList/>
+      </Row>
+    </Container>
+  </section>
+
+  {/*===================== featured tour or timeshare start==============*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12' className='mb-5'>
+          <Subtitle subtitle={'Explore'}/>
+          <h2 className='featured__tour-title'> Our featured tours</h2>
+        </Col>
+        <FeaturedTourList />
+      </Row>
+    </Container>
+  </section>
+  {/*===================== featured tour or timeshare end==============*/}
+  {/*===================== experience section tour or timeshare start==============*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+          <div className="experience__content">
+            <Subtitle subtitle={'Experience'}/>
+            <h2>With our all experience <br/> we will serve you
+            </h2>
+            <p>
+            We are always there to serve the needs of renting <br /> and exchanging timeshares with each other
+            </p>
+          </div>
+          <div className="counter__wrapper d-flex align-items-center gap-5">
+            <div className="counter__box">
+              <span>10k+</span>
+              <h6>Successfull Timeshare</h6>
+            </div>
+            <div className="counter__box">
+              <span>2k+</span>
+              <h6>Regular clients</h6>
+            </div>
+            <div className="counter__box">
+              <span>2</span>
+              <h6>Years experience</h6>
+            </div>
+          </div>
+        </Col>
+        <Col lg='6'>
+          <div className="experience__img">
+            <img src={experienceImg} alt="" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  {/*===================== experience section tour or timeshare end==============*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12'>
+          <Subtitle subtitle={'Fans Love'} />
+          <h2 className="testimonial__title">
+            What our fans say about us
+          </h2>
+        </Col>
+        <Col lg='12'>
+          <Testimonial />
         </Col>
       </Row>
     </Container>
